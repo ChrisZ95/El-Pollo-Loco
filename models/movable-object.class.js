fda06fb2de41fe4023ae8ce8c1fa3f1c846f0860
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-03-23 08:56:09 +0100

    first commit

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,13 +1,37 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 280;
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
+    otherDirection = false;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path
+            this.imageCache[path] = img;
+        });
+    }
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length // let i = 7 % 6; => 1, Rest 1
+        // i = 0, 1, 2, 3, 4, 5, 6, 0, 1, ...
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;  
     }
 
     moveRight() {
@@ -15,6 +39,8 @@ class MovableObject {
     }
 
     moveLeft() {
-
+        setInterval( () => {
+            this.x -= this.speed;
+            }, 1000 / 60);
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,31 +1,59 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
+    level = level1;
     canvas;
     ctx;
+    keyboard;
+    camera_x = 0;
 
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
        this.ctx = canvas.getContext('2d');
        this.canvas = canvas;
+       this.keyboard = keyboard;
        this.draw(); 
+       this.setWorld();
+    }
+
+    setWorld() {
+        this.character.world = this;
     }
 
     // Draw wird immer wieder aufgerufen, so viel es die Grafikkarte hergibt.
     draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Canvas wird gelöscht
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.ctx.translate(this.camera_x, 0);
+
+        this.addObjectsToMap(this.level.backgroundObjects); //Objekte werden hinzugefügt
+        this.addToMap(this.character); //Objekte werden hinzugefügt
+        this.addObjectsToMap(this.level.enemies); //Objekte werden hinzugefügt
+        this.addObjectsToMap(this.level.clouds); //Objekte werden hinzugefügt
+
+        this.ctx.translate(-this.camera_x, 0);
         
         let self = this; //Innerhalb der request Funktion wird die Welt nicht mehr erkannt, daher müssen wir das Wort mit einer Variablen umgehen.
         requestAnimationFrame(function() { //Funktion wird ausgeführt, wenn alles darüber fertig gezeichnet wurde.
           self.draw();  
         });
     }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        if(mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0); // Verursacht das Verschieben
+            this.ctx.scale(-1, 1); // Verursacht die Spiegelung des Charakters
+            mo.x = mo.x * -1;
+        }
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if(mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
+    };
 }
